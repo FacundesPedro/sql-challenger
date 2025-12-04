237265c3a0c5f34f72467d26029b076008971ea8
FacundesPedro <pedro.f.facundes1@gmail.com>
2025-12-04 11:43:22 -0300

    [feat/refactor]: creating mystery page, sqlEditor, mysteryHints, misteryDatabaseexplorer and resultsPanel

--- _components/_organisms/header.tsx
+++ _components/_organisms/header.tsx
@@ -13,15 +13,17 @@ export function Header() {
     // const {user} = useUser()
 
     return (
-        <header className="rounded-xl sticky top-2 mx-4 z-50 w-full border-b border-border bg-background/85 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-            <div className="container mx-auto flex h-16 items-center justify-between px-4">
-                <Logo size="md"/>
+        <div className="sticky top-2 z-50 w-full px-4">
+            <header className="rounded-xl border-b border-border bg-background/85 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+                <div className="container mx-auto flex h-16 items-center justify-between px-4">
+                    <Logo size="md" />
 
-                <div className="flex items-center gap-2">
-                    <ThemeToggler />
-                    <UserMenu />
+                    <div className="flex items-center gap-2">
+                        <ThemeToggler />
+                        <UserMenu />
+                    </div>
                 </div>
-            </div>
-        </header>
+            </header>
+        </div>
     )
 }
